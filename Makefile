f5abce69219616edf91a721320486e89d49662d6
inesribeiro <A52852@alunos.isel.pt>
2026-03-17 23:31:33 +0000

    makefile - ex2

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-BINS = ex1 ex2 vector-seq ex3
+BINS = count_words 
 
 CC       = gcc
 CFLAGS  += -Wall    # warnings básicos
@@ -7,17 +7,20 @@ CFLAGS  += -Werror  # transforma warnings em erros
 
 all: $(BINS)
 
-ex1: ex1.o
-ex1.o: ex1.c
+#ex1: ex1.o
+#ex1.o: ex1.c
 	
 ex2: ex2.o
 ex2.o: ex2.c
+
+count_words: count_words.o
+	$(CC) $(CFLAGS) -o count_words count_words.o
 		
-vector-seq: vector-seq.o
-vector-seq.o: vector-seq.c
+#vector-seq: vector-seq.o
+#vector-seq.o: vector-seq.c
 	
-ex3: ex3.o
-ex3.o: ex3.c
+#ex3: ex3.o
+#ex3.o: ex3.c
 
 clean:
 	$(RM) $(BINS) *.o
--- count_words.c
+++ count_words.c
@@ -11,43 +11,65 @@ void fatal_system_error(const char *errorMsg) {
 
 int main(int argc, char *argv[]) {
 
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+    if (argc != 2) { //programa deve receber 2 args
+        fprintf(stderr, "Uso: %s <ficheiro>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    int fd[2];
+    int fd[2]; // Array para guardar os descritores do pipe
 
-    if (pipe(fd) == -1)
-        fatal_system_error("pipe");
+    if (pipe(fd) == -1) //se falhar retorna -1
+        fatal_system_error("Erro no pipe");
 
-    pid_t pid = fork();
+    pid_t pid = fork(); //cria um novo processo, processo filho
 
-    if (pid < 0)
-        fatal_system_error("fork");
+    if (pid < 0) //erro ao criar processo, não sei se necessário 
+        fatal_system_error("Erro no fork");
 
-    if (pid == 0) {
+    if (pid == 0) { //processo filho 
 
-        close(fd[0]); // filho não lê
+        close(fd[0]); //O filho não vai ler do pipe. Então fechamos a leitura
 
-        dup2(fd[1], STDOUT_FILENO);
+        if (dup2(fd[1], STDOUT_FILENO) == -1) //Redireciona o stdout para o pipe, tudo o que o filho imprimir vai para o pipe
+            fatal_system_error("Erro no dup2");
+
         close(fd[1]);
 
-        execlp("wc", "wc", "-w", argv[1], NULL);
+        execlp("wc", "wc", "-w", argv[1], (char *)NULL); //substitui o processo atual, o filho passa a ser o programa wc
 
-        fatal_system_error("exec");
+        fatal_system_error("Erro ao executar wc");
     }
 
-    close(fd[1]); // pai não escreve
+    close(fd[1]); // o pai nao escreve no pipe, fecha a escrita
 
     char buffer[128];
-    read(fd[0], buffer, sizeof(buffer));
+    ssize_t n = read(fd[0], buffer, sizeof(buffer) - 1); //Lê dados do pipe
 
-    printf("Number of words: %s", buffer);
+    if (n < 0)
+        fatal_system_error("Erro na leitura do pipe");
 
-    close(fd[0]);
+    buffer[n] = '\0';
 
-    waitpid(pid, NULL, 0);
+    close(fd[0]); //Fecha o pipe
 
-    return 0;
+    int status;
+
+    if (waitpid(pid, &status, 0) == -1) // o pai espera o processo filho terminar. Evita processos zombie
+        fatal_system_error("Erro no waitpid");
+
+    if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) { //verifica se o wc terminou corretamente
+        fprintf(stderr, "Erro: o comando wc terminou com erro.\n");
+        return EXIT_FAILURE;
+    }
+
+    long palavras;
+
+    if (sscanf(buffer, "%ld", &palavras) != 1) {
+        fprintf(stderr, "Erro: nao foi possivel obter o numero de palavras.\n");
+        return EXIT_FAILURE;
+    }
+
+    printf("%ld\n", palavras);
+
+    return EXIT_SUCCESS;
 }
