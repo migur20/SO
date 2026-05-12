2841bb0e6a918d12270484f7cb2ef6bd1880c53b
migur20 <miguelraposo2006@gmail.com>
2026-05-12 16:13:37 +0100

    trab3 ex1

--- trab2/ex1/server.c
+++ trab2/ex1/server.c
@@ -1,30 +1,43 @@
 #include "common.h"
+#include <pthread.h>
 #include <unistd.h>
 
-int main() {
+void *unix_func(void *args)
+{
+  int sock_unix;
+  sock_unix = create_unix_socket();
+  listen(sock_unix, 5);
+  printf("[UNIX]: Listening on path \"%s\"\n", SOCKET_PATH);
+
+  int clientfd;
+
+  while (1) {
+    clientfd = accept(sock_unix, NULL, NULL);
+    printf("[UNIX]: Received client on fd:%d\n", clientfd);
+
+    handle_client(clientfd);
+    printf("[UNIX]: Client on fd:%d handled\n", clientfd);
+
+    close(clientfd);
+  }
+  close(sock_unix);
+  unlink(SOCKET_PATH);
+  printf("[UNIX]: Closed Unix socket\n");
+}
+
+void *inet_func(void *args)
+{
+}
+
+int main()
+{
+  pthread_t unix_thread;
+  pthread_create(&unix_thread, NULL, unix_func, NULL);
+
   pid_t pid = fork();
 
   if (pid == 0) {
     // FILHO → UNIX
-    int sock_unix;
-    sock_unix = create_unix_socket();
-    listen(sock_unix, 5);
-    printf("[UNIX]: Listening on path \"%s\"\n", SOCKET_PATH);
-
-    int clientfd;
-
-    while (1) {
-      clientfd = accept(sock_unix, NULL, NULL);
-      printf("[UNIX]: Received client on fd:%d\n", clientfd);
-
-      handle_client(clientfd);
-      printf("[UNIX]: Client on fd:%d handled\n", clientfd);
-
-      close(clientfd);
-    }
-    close(sock_unix);
-		unlink(SOCKET_PATH);
-    printf("[UNIX]: Closed Unix socket\n");
   } else {
     // PAI → INET
     int sock_inet;
